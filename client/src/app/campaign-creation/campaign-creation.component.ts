69a2421499674b6730787c2480105f5df89b97f4
Mikko Karttunen <mikko.t.karttunen@gmail.com>
2017-08-07 14:18:22 +0300

    Campaign creation form inputs completed

--- client/src/app/campaign-creation/campaign-creation.component.ts
+++ client/src/app/campaign-creation/campaign-creation.component.ts
@@ -10,11 +10,26 @@ import { Campaign } from '../campaign';
 })
 export class CampaignCreationComponent {
   campaign = new Campaign();
+  item: string;
 
   constructor(
     private backendService: BackendService
   ) {}
 
   ngOnInit(): void {
+    this.campaign.private = false;
+    this.campaign.initialItems = ["Sword", "Backpack", "Lantern"];
+    this.item = "";
+  }
+
+  addItem(): void {
+    if(this.item != "") {
+      this.campaign.initialItems.push(this.item);
+      this.item = "";
+    }
+  }
+
+  deleteItem(toDelete: string): void {
+    this.campaign.initialItems.splice(this.campaign.initialItems.indexOf(toDelete), 1);
   }
 }
